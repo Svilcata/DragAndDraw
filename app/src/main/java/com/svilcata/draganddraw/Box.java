1489911f901aacbfb25ed7c354153923d0680700
Svilcata <lord_solen@abv.bg>
2016-05-12 01:51:37 +0300

    Chapter 29

---
+++ app/src/main/java/com/svilcata/draganddraw/Box.java
@@ -0,0 +1,28 @@
+package com.svilcata.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by Svilcata on 12-May-16.
+ */
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
